de7cd09d7a235fc579c37667b935db26b2ae3d3f
Git~ <555+git@admin.noreply.github.com>
2019-04-14 18:58:04 +0700

    upup

--- www/scripts/home.js
+++ www/scripts/home.js
@@ -28,9 +28,7 @@ request.onload = () => {
 			var category = res.data[i].category[0].name;
 			var cover = res.data[i].coverlite;
 			var covercard = res.data[i].coverlite1;
-			var coverc = res.data[i].coverlite2;
 			var date = res.data[i].published;
-			var id = res.data[i].id;
 			var title = res.data[i].title;
 			var user = res.data[i].user.fullname;
 			var minides = res.data[i].seo_description;
@@ -55,7 +53,7 @@ request.onload = () => {
 						<span class="article-overlay"></span>
 						<img class="article-image preload-image" src="` + covercard + `">
 						<span class="article-category bg-highlight color-white uppercase">` + category + `</span>
-						<span class="article-author color-gray-light"><i class="fa fa-user opacity-50"></i>"+id+"</span>
+						<span class="article-author color-gray-light"><i class="fa fa-user opacity-50"></i>`+user+`</span>
 						<span class="article-time color-gray-light">` + date + `<i class="fas fa-clock opacity-50"></i></span>
 					</a>
 					<div class="article-content">
